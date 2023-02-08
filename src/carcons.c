82557197201ba3c37b1b9eeb2fed6c8c629bbc9e
AndrewCRMartin <andrew@bioinf.org.uk>
2023-02-08 16:42:51 +0000

    Added all maths functions and skeleton main

--- src/carcons.c
+++ src/carcons.c
@@ -42,6 +42,8 @@
 
    Usage:
    ======
+   Compile with -std=c99 to activate inlining
+
 
 **************************************************************************
 
@@ -54,12 +56,17 @@
 */
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 #include "bioplib/MathType.h"
+#include "bioplib/SysDefs.h"
 #include "bioplib/macros.h"
+#include "bioplib/general.h"
 
 /************************************************************************/
 /* Defines and macros
 */
+#define MAXBUFF 512
+
 typedef struct _seqlist
 {
    char *sequence;
@@ -67,7 +74,13 @@ typedef struct _seqlist
    struct _seqlist *next;
 }  SEQLIST;
 
-   
+/* If compiled with -std=c99 some functions will be inlined             */
+#ifdef __GNUC_STDC_INLINE__
+#   define INLINE static inline
+#else
+#   define INLINE static
+#endif
+
    
 
 /************************************************************************/
@@ -78,30 +91,135 @@ typedef struct _seqlist
 /* Prototypes
 */
 int  main(int argc, char **argv);
-REAL ScorePairwiseAlignmentIdentity(char *seq1, char *seq2);
+REAL ScorePairwiseAlignmentIdentity(char *seq1, char *seqJ);
+REAL SDWeightedDiversity(SEQLIST *seqlist, int nSeqs, int position,
+                         REAL meanWeightedDiversity);
 REAL MeanWeightedDiversity(SEQLIST *seqlist, int nSeqs, int position);
-REAL ScoreDiversity(char *seq1, char *seq2, int position);
-REAL ScoreSimilarity(char *seq1, char *seq2, int position);
+INLINE REAL ScoreDiversity(char *seq1, char *seqJ, int position);
+INLINE REAL ScoreSimilarity(char *seq1, char *seqJ, int position);
 REAL MDMScore(char aa1, char aa2);
+INLINE REAL CalculateThreshold(REAL meanWeightedDiversity,
+                        REAL sdWeightedDiversity,
+                        REAL numSD);
+REAL ScoreSelfIDMutant(char *seq, int nMutant);
+BOOL ParseCmdLine(int argc, char **argv, char *infile, char *outfile, 
+                  int *position, char *mutation, REAL *numSD);
+void Usage(void);
 
 
 
 /************************************************************************/
 int main(int argc, char **argv)
 {
+   FILE    *in  = stdin,
+           *out = stdout;
+   char    inFile[MAXBUFF],
+           outFile[MAXBUFF],
+           mutation;
+   REAL    numSD    = 2.0;
+   int     position = 0;
+   
+   if(ParseCmdLine(argc, argv, inFile, outFile, &position, &mutation,
+                   &numSD))
+   {
+      if(blOpenStdFiles(inFile, outFile, &in, &out))
+      {
+         
+      }
+      else
+      {
+         fprintf(stderr,"carcons: Error - unable to open input or \
+output file\n");
+         return(1);
+
+      }
+   }
+   else
+   {
+      Usage();
+   }
+   
    return(0);
    
 }
 
 /************************************************************************/
+BOOL ParseCmdLine(int argc, char **argv, char *infile, char *outfile, 
+                  int *position, char *mutation, REAL *numSD)
+{
+   argc--;
+   argv++;
+
+   infile[0] = outfile[0] = '\0';
+   
+   while(argc)
+   {
+      if(argv[0][0] == '-')
+      {
+         switch(argv[0][1])
+         {
+         case 'n':
+            argc--;
+            argv++;
+            if(!sscanf(argv[0], "%lf", numSD))
+               return(FALSE);
+            break;
+         default:
+            return(FALSE);
+            break;
+         }
+      }
+      else
+      {
+         /* Check that there are 2-4 <= 2 arguments left                */
+         if((argc < 2) || (argc > 4))
+            return(FALSE);
+
+         /* Copy the first (required) to position                       */
+         if(!sscanf(argv[0], "%d", position))
+            return(FALSE);
+         argc--;
+         argv++;
+
+         /* Copy the second (required) to mutation                      */
+         *mutation = argv[0][0];
+        
+         /* If there is another, copy the first to infile               */
+         argc--;
+         argv++;
+         if(argc)
+         {
+            strcpy(infile, argv[0]);
+         }
+         
+         /* If there is another, copy the second to outfile             */
+         argc--;
+         argv++;
+         if(argc)
+         {
+            strcpy(infile, argv[0]);
+         }
+         
+         return(TRUE);
+      }
+      argc--;
+      argv++;
+   }
+   
+   return(TRUE);
+}
+
+
+
+/************************************************************************/
 REAL MeanWeightedDiversity(SEQLIST *seqlist, int nSeqs, int position)
 {
    SEQLIST *s;
-   int j, k;
-   REAL Vr = (REAL)0.0,
-      P_1j,
-      v_1j;
-   char *seq1, *seq2;
+   int     j, k;
+   REAL    Vr = (REAL)0.0,
+           P_1j,
+           v_1j;
+   char    *seq1, *seqJ;
    
 
    seq1 = seqlist->sequence;
@@ -114,10 +232,10 @@ REAL MeanWeightedDiversity(SEQLIST *seqlist, int nSeqs, int position)
       {
          NEXT(s);
       }
-      seq2 = s->sequence;
+      seqJ = s->sequence;
       
-      P_1j = ScorePairwiseAlignmentIdentity(seq1, seq2);
-      v_1j = ScoreDiversity(seq1, seq2, position);
+      P_1j = ScorePairwiseAlignmentIdentity(seq1, seqJ);
+      v_1j = ScoreDiversity(seq1, seqJ, position);
       
       Vr += (v_1j * P_1j);
    }
@@ -128,49 +246,139 @@ REAL MeanWeightedDiversity(SEQLIST *seqlist, int nSeqs, int position)
 }
 
 /************************************************************************/
-REAL ScoreDiversity(char *seq1, char *seq2, int position)
+INLINE REAL CalculateThreshold(REAL meanWeightedDiversity,
+                                      REAL sdWeightedDiversity,
+                                      REAL numSD)
+{
+   return(meanWeightedDiversity + (numSD * sdWeightedDiversity));
+}
+
+
+
+/************************************************************************/
+REAL CalculateDelta(SEQLIST *seqlist, int position, char mutant,
+                    REAL threshold)
+{
+   REAL P_1M,
+        v_1M;
+   char native;
+
+   native = seqlist->sequence[position];
+
+   v_1M = 1 - (MDMScore(native, mutant)/10.0);
+   P_1M = ScoreSelfIDMutant(seqlist->sequence, 1);
+   
+   return(threshold - (v_1M * P_1M));
+}
+
+
+/************************************************************************/
+REAL SDWeightedDiversity(SEQLIST *seqlist, int nSeqs, int position,
+                         REAL meanWeightedDiversity)
+{
+   char *seq1, *seqJ;
+   int  j, k;
+   
+   REAL sumDevSq = 0.0,
+        dev, sd,
+        P_1j,
+        v_1j;
+   seq1 = seqlist->sequence;
+   
+   for(j=1; j<nSeqs; j++)
+   {
+      SEQLIST *s;
+      
+      s = seqlist;
+      for(k=0; k<=j; k++)
+      {
+         NEXT(s);
+      }
+      seqJ = s->sequence;
+      
+      P_1j = ScorePairwiseAlignmentIdentity(seq1, seqJ);
+      v_1j = ScoreDiversity(seq1, seqJ, position);
+
+      dev = ((v_1j * P_1j) - meanWeightedDiversity);
+      sumDevSq += (dev*dev);
+   }
+
+   sd = sqrt(sumDevSq/(nSeqs-1));
+
+   return(sd);
+}
+
+
+
+/************************************************************************/
+INLINE REAL ScoreDiversity(char *seq1, char *seqJ, int position)
 {
    REAL similarity;
    
-   similarity = ScoreSimilarity(seq1, seq2, position);
+   similarity = ScoreSimilarity(seq1, seqJ, position);
    return(1-similarity);
 }
 
 /************************************************************************/
-REAL ScoreSimilarity(char *seq1, char *seq2, int position)
+INLINE REAL ScoreSimilarity(char *seq1, char *seqJ, int position)
 {
-   return(MDMScore(seq1[position], seq2[position])/10.0);
+   return(MDMScore(seq1[position], seqJ[position])/10.0);
 }
 
 /************************************************************************/
-REAL ScorePairwiseAlignmentIdentity(char *seq1, char *seq2)
+REAL ScorePairwiseAlignmentIdentity(char *seq1, char *seqJ)
 {
-   int NPos   = 0,
-      NMatch = 0,
-      seqLen,
-      i;
+   int nPos   = 0,
+       nMatch = 0,
+       seqLen,
+       i;
 
-   seqLen = MIN(strlen(seq1), strlen(seq2));
+   seqLen = MIN(strlen(seq1), strlen(seqJ));
 
    for(i=0; i<seqLen; i++)
    {
-      if(seq1[i] == seq2[i])
+      if(seq1[i] == seqJ[i])
       {
          /* It's a match                                                */
          if(seq1[i] != '-')     /* It's not a double insertion          */
          {
-            NPos++;
-            NMatch++;
+            nPos++;
+            nMatch++;
          }
       }
       else
       {
          /* It's not a match                                            */
-         NPos++;
+         nPos++;
       }
    }
    
-   return((REAL)NMatch / (REAL)NPos);
+   return((REAL)nMatch / (REAL)nPos);
+}
+/************************************************************************/
+REAL ScoreSelfIDMutant(char *seq, int nMutant)
+{
+   int nPos   = 0,
+       nMatch = 0,
+       seqLen,
+       i;
+
+   seqLen = strlen(seq);
+
+   for(i=0; i<seqLen; i++)
+   {
+      if(seq[i] != '-')     /* It's not an insertion                    */
+      {
+         nPos++;
+         nMatch++;
+      }
+   }
+   
+   return((REAL)(nMatch-nMutant) / (REAL)nPos);
+}
+
+void Usage(void)
+{
 }
 
 
